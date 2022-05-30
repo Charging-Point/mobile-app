0fef3f35f972010138a2f1afb1974f9456141b9c
7Kenz <kenz.narainen@epita.fr>
2022-05-30 21:42:49 +0200

    [FEATURE] New UI Design

--- screens/ConnectorScreen.js
+++ screens/ConnectorScreen.js
@@ -1,29 +1,55 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+
 
 export default function ConnectorScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          title="USB C"
-          onPress={() => navigation.navigate('ScanDeposit', {connector: 'usb-c'})}
-        />
-        <View style={styles.space} />
-        <Button
-          title="iPhone"
-          onPress={() => navigation.navigate('ScanDeposit', {connector: 'iphone'})}
-        />
-        <View style={styles.space} />
-        <Button
-          title="Micro USB"
-          onPress={() => navigation.navigate('ScanDeposit', {connector: 'micro-usb'})}
-        />
-      </View>
-    );
-  }
 
-  const styles = StyleSheet.create({
-    space: {
-      width: 20,
-      height: 20,
-    },
-  });
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [navigation])
+
+  return (
+    <View style={{ flex: 1, paddingVertical: 20, paddingHorizontal: 15 }}>
+      <TouchableOpacity style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }} onPress={() => navigation.goBack()}>
+        <Image source={require('../assets/images/arrowBack.png')} style={{ width: 25, height: 25 }} />
+        <Text style={{ fontSize: 20, fontWeight: '500', marginLeft: 10 }}>Back</Text>
+      </TouchableOpacity>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', paddingHorizontal: 25, marginTop: 20 }}>Choose Connector :</Text>
+      <View style={{ flex: 9, justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginTop: 20 }}>
+
+        <TouchableOpacity style={styles.btnConnector} onPress={() => navigation.navigate('ScanDeposit', { connector: 'usb-c' })}>
+          <Image source={require('../assets/images/typeC.png')} style={{ width: 60, height: 60 }} />
+          <Text style={{ fontSize: 15, fontWeight: 'bold', width: '100%', textAlign: 'center' }}>USB Type-C</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnConnector} onPress={() => navigation.navigate('ScanDeposit', { connector: 'iphone' })}>
+          <Image source={require('../assets/images/iphone.png')} style={{ width: 60, height: 60 }} />
+          <Text style={{ fontSize: 15, fontWeight: 'bold', width: '100%', textAlign: 'center' }}>Iphone</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnConnector} onPress={() => navigation.navigate('ScanDeposit', { connector: 'micro-usb' })}>
+          <Image source={require('../assets/images/microUsb.png')} style={{ width: 60, height: 60 }} />
+          <Text style={{ fontSize: 15, fontWeight: 'bold', width: '100%', textAlign: 'center' }}>Micro USB</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  space: {
+    width: 20,
+    height: 20,
+  },
+  btnConnector: {
+    height: 200,
+    width: '40%',
+    margin: 10,
+    borderWidth: 3,
+    borderColor: "#84CEFF",
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  }
+});
--- screens/PhonePickupScreen.js
+++ screens/PhonePickupScreen.js
@@ -1,38 +1,60 @@
 import { Button, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 
 
 export default function PhonePickupScreen({ route, navigation }) {
-  const {id_locker, user_uid} = route.params;
+  const { id_locker, user_uid } = route.params;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [navigation])
 
   const updateLockerState = async () => {
     try {
-    const requestOptions = {
+      const requestOptions = {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-    };
-     const response = await fetch('http://35.180.116.112:5000/locker?' + new URLSearchParams({id_locker: id_locker, new_state : 0}), requestOptions);
-     const json = await response.json();
-     if (json.result == 1){
-        navigation.navigate('ChoiceLetCharge', {id_locker: id_locker, user_uid: user_uid, deposit_time: json.deposit_time})
-     }
-     else {
+      };
+      const response = await fetch('http://35.180.116.112:5000/locker?' + new URLSearchParams({ id_locker: id_locker, new_state: 0 }), requestOptions);
+      const json = await response.json();
+      if (json.result == 1) {
+        navigation.navigate('ChoiceLetCharge', { id_locker: id_locker, user_uid: user_uid, deposit_time: json.deposit_time })
+      }
+      else {
         navigation.navigate('Error')
-     }
-   } catch (error) {
-        console.error(error);
-   }
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Récupérez le casier dans le casier {id_locker}</Text>
-        <Button
-          title="C'est fait"
-          onPress={updateLockerState}
-        />
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ alignItems: 'center', justifyContent: 'center', width: '100%', marginBottom: 50 }} >
+        <Text style={{ fontSize: 15, fontWeight: '600', textAlign: 'center', color: '#5C5C5C', width: '100%' }}>Récupérez le Téléphone dans le casier : </Text>
+        <Text style={{ fontSize: 70, fontWeight: '500', textAlign: 'center', color: '#3DAAF2', width: '100%' }}>{id_locker}</Text>
       </View>
-    );
-  }
+      <TouchableOpacity style={styles.btnDone} onPress={updateLockerState}>
+        <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold', textAlign: 'center', width: '100%' }}>C'est Fait</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
-  
+const styles = StyleSheet.create({
+  space: {
+    width: 20,
+    height: 20,
+  },
+  btnDone: {
+    width: 180,
+    height: 50,
+    backgroundColor: 'rgba(1, 185, 63, 0.64)',
+    borderRadius: 5,
+    alignContent: 'center',
+    justifyContent: 'center'
+  }
+});
+
